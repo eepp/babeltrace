943b6faf5a28693b34b919a43d9346ad8bc8fcc1
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2015-03-05 21:14:10 -0500

    Revert "Don't force a sequence's length field to be set on creation"
    This reverts commit e316320161fd036afad66ac8df4ea8d41e782ea6.

--- formats/ctf/ir/event-types.c
+++ formats/ctf/ir/event-types.c
@@ -1555,9 +1555,7 @@ struct bt_ctf_field_type *bt_ctf_field_type_sequence_create(
 {
 	struct bt_ctf_field_type_sequence *sequence = NULL;
 
-	if (!element_type ||
-		(length_field_name &&
-			bt_ctf_validate_identifier(length_field_name)) ||
+	if (!element_type || bt_ctf_validate_identifier(length_field_name) ||
 		bt_ctf_field_type_validate(element_type)) {
 		goto error;
 	}
@@ -1610,8 +1608,7 @@ const char *bt_ctf_field_type_sequence_get_length_field_name(
 
 	sequence = container_of(type, struct bt_ctf_field_type_sequence,
 		parent);
-	ret = sequence->length_field_name->len ?
-		sequence->length_field_name->str : NULL;
+	ret = sequence->length_field_name->str;
 end:
 	return ret;
 }
