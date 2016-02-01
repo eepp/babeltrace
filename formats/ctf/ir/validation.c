95a4c7142c4fa9c9644daff3bbf124e5c7896b14
Philippe Proulx <eeppeliteloop@gmail.com>
2016-02-01 08:15:24 -0500

    [tmp] validation

--- formats/ctf/ir/validation.c
+++ formats/ctf/ir/validation.c
@@ -204,7 +204,6 @@ int bt_ctf_validate_class_types(struct bt_ctf_field_type *packet_header_type,
 		}
 
 		/* Put original reference and move copy */
-		BT_PUT(packet_header_type);
 		BT_MOVE(packet_header_type, packet_header_type_copy);
 
 		/* Validate trace field types */
@@ -251,9 +250,6 @@ int bt_ctf_validate_class_types(struct bt_ctf_field_type *packet_header_type,
 		}
 
 		/* Put original references and move copies */
-		BT_PUT(packet_context_type);
-		BT_PUT(event_header_type);
-		BT_PUT(stream_event_ctx_type);
 		BT_MOVE(packet_context_type, packet_context_type_copy);
 		BT_MOVE(event_header_type, event_header_type_copy);
 		BT_MOVE(stream_event_ctx_type, stream_event_ctx_type_copy);
@@ -304,8 +300,6 @@ sc_validation_done:
 		}
 
 		/* Put original references and move copies */
-		BT_PUT(event_context_type);
-		BT_PUT(event_payload_type);
 		BT_MOVE(event_context_type, event_context_type_copy);
 		BT_MOVE(event_payload_type, event_payload_type_copy);
 
@@ -363,14 +357,10 @@ void bt_ctf_validation_replace_types(struct bt_ctf_trace *trace,
 		enum bt_ctf_validation_flag replace_flags)
 {
 	if ((replace_flags & BT_CTF_VALIDATION_FLAG_TRACE) && trace) {
-		BT_PUT(trace->packet_header_type);
 		BT_MOVE(trace->packet_header_type, output->packet_header_type);
 	}
 
 	if ((replace_flags & BT_CTF_VALIDATION_FLAG_STREAM) && stream_class) {
-		BT_PUT(stream_class->packet_context_type);
-		BT_PUT(stream_class->event_header_type);
-		BT_PUT(stream_class->event_context_type);
 		BT_MOVE(stream_class->packet_context_type,
 			output->packet_context_type);
 		BT_MOVE(stream_class->event_header_type,
@@ -380,8 +370,6 @@ void bt_ctf_validation_replace_types(struct bt_ctf_trace *trace,
 	}
 
 	if ((replace_flags & BT_CTF_VALIDATION_FLAG_EVENT) && event_class) {
-		BT_PUT(event_class->context);
-		BT_PUT(event_class->fields);
 		BT_MOVE(event_class->context, output->event_context_type);
 		BT_MOVE(event_class->fields, output->event_payload_type);
 	}
