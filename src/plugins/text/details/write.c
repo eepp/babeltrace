a5e14c381ee15acdeba614d4d7412dc3fd9a3353
Philippe Proulx <eeppeliteloop@gmail.com>
2019-06-14 17:29:56 -0400

    Logging: standardize logging tags
    Use `/` to show the subsystem to source file hierarchy, and use typical
    component class identifier, but in uppercase (for example,
    `SRC.CTF.FS`).
    
    See the changes in `CONTRIBUTING.adoc` for more details.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I94aaf431e6a93e607496267c54419b012564daaf

--- src/plugins/text/details/write.c
+++ src/plugins/text/details/write.c
@@ -20,10 +20,8 @@
  * SOFTWARE.
  */
 
-#define BT_LOG_TAG "PLUGIN-TEXT-DETAILS-SINK-WRITE"
-#include "logging.h"
-
 #include <babeltrace2/babeltrace.h>
+#include <stdio.h>
 
 #include "common/assert.h"
 #include "common/common.h"
